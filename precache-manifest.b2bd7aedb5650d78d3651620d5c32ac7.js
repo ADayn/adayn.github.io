841ec55fb28d470de6cd252ce2b1f5d18f444160
Albert Dayn <Albert.Dayn@Colorado.edu>
2020-06-22 15:29:47 -0600

    Added resume site

---
+++ precache-manifest.b2bd7aedb5650d78d3651620d5c32ac7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c308960048cc1a7c062a5a1fbed9b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f384dc08bbf1c329dc",
+    "url": "/static/css/main.68c879f8.chunk.css"
+  },
+  {
+    "revision": "27ad8063b7222510c35c",
+    "url": "/static/js/2.2dd4375b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2dd4375b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f384dc08bbf1c329dc",
+    "url": "/static/js/main.d437df3c.chunk.js"
+  },
+  {
+    "revision": "cecac9fa101fa5d04b6f",
+    "url": "/static/js/runtime-main.c94c6b64.js"
+  },
+  {
+    "revision": "172baea2a5af79439ef4a44af0b33119",
+    "url": "/static/media/al.172baea2.jpg"
+  },
+  {
+    "revision": "b6d05c27874161c1374b95d740ce9355",
+    "url": "/static/media/bologna_square.b6d05c27.jpg"
+  },
+  {
+    "revision": "106e4846a8ab093e0504ab2f39e407bd",
+    "url": "/static/media/eclairs_square.106e4846.jpg"
+  },
+  {
+    "revision": "b265d5c4ccecaa442f21c1f67bd42e46",
+    "url": "/static/media/resume.b265d5c4.pdf"
+  },
+  {
+    "revision": "106e5a4aa6d05422dafaaa5a916031f9",
+    "url": "/static/media/shramps_square.106e5a4a.mp4"
+  }
+]);
